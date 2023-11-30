8e30433fda272263911186a426d9a6fa233f7944
almeidabbm <almeidabatistam@gmail.com>
2023-11-30 15:56:19 +0000

    fix: remove enforce from services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,16 +1,7 @@
 import * as gcp from './gcp';
 import * as aws from './aws';
-import CloudProviders from '../utils/cloud-providers';
 
-type Services = {
-  readonly [k in CloudProviders]: {
-    [j in string]: unknown;
-  };
-};
-
-const services: Services = {
+export default {
   aws,
   gcp,
 };
-
-export default services;
